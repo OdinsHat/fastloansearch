4f91cd85bdc2605cb391241674de3a122a107d06
Doug Bromley <doug.bromley@gmail.com>
2014-05-26 00:11:01 +0100

    Removing debug toolbar

--- fastloansearch.py
+++ fastloansearch.py
@@ -1,18 +1,10 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 import requests
-from flask_debugtoolbar import DebugToolbarExtension
 from database import db_session
 from models import Result, Product
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY = 'uytjytfuytviti7f8tvwkuvfigy348u'
-))
-
-toolbar = DebugToolbarExtension(app)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):

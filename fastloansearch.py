c92aaf458ff18e60f9391efd2618e5e11b6ddb85
Doug Bromley <doug.bromley@gmail.com>
2014-05-25 21:31:56 +0100

    Including requests lib so we can call Google CSE API

--- fastloansearch.py
+++ fastloansearch.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+import requests
 
 app = Flask(__name__)
 app.config.from_object(__name__)

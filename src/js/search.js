5388b3d629af2a4692c93d14c99229bfdd513ef6
Doug Bromley <doug.bromley@gmail.com>
2014-06-13 00:09:02 +0100

    Tidying up the JS

--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,5 @@
 (function ($) {
     'use strict';
-    /**
-     * Created by doug on 21/03/2014.
-     */
 
     function runSearch() {
         var formdata = $('#frm-quick-search').serialize();
@@ -14,28 +11,6 @@
             success: returnResults
         });
         return false;
-    }
-
-    function returnResults(data) {
-        var resultitems = data.items;
-        $.each(resultitems, function(k, v){
-            recursiveResult(k, v);
-        });
-
-        /*
-        var resulthtml = '';
-        var count = resultitems.length;
-        for (i = 0; i < count; i++) {
-            resulthtml += '<article class="result-' + i + '" style="opacity:0">';
-            resulthtml += '<h3><a hrf="' + resultitems[i].link + '">' + resultitems[i].title + '</a></h3>';
-            resulthtml += '<p>' + resultitems[i].snippet + '</p>';
-            resulthtml += '<small>' + resultitems[i].displayLink + '</small>';
-            resulthtml += '</article>';
-            $('#results').append(resulthtml);
-            $('.result-' + i).addClass('animated bounceInUp');
-            resulthtml = '';
-        }
-        */
     }
 
     function recursiveResult(key, val){

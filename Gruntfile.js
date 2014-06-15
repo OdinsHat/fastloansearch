747407973939890a79f65867fe654dad0df89dce
Doug Bromley <doug.bromley@gmail.com>
2014-06-15 23:35:14 +0100

    Added grunt-concurrent to tasks - ~1s speed up

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concurrent: {
+            target1: ['cssmin', 'imagemin'],
+            target2: ['copy', 'jshint']
+        },
         jshint: {
             options: {
                 reporter: require('jshint-stylish')
@@ -107,6 +111,7 @@ module.exports = function(grunt) {
         }
    });
 
+    grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
